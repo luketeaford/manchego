016caac15cef3dba1117807d2e2d5101c06e58ac
Luke Teaford <luketeaford@gmail.com>
2019-03-03 08:23:04 -0500

    Write better description for part test

--- test/part.test.js
+++ test/part.test.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const part = require('../lib/part')
 
-test('The part function takes an array and returns a function that takes a filter. The resulting function returns an object containing a pass key with all the items in the array the filter passed and a fail key with all the items in the array the filter rejected.', t => {
+test('The part function takes an array and returns a function that takes a filter. The resulting function returns an object containing a pass array with all the items the filter passed and a fail array with all the items the filter rejected.', t => {
   const someArray = ['a', 1, 0, 'b', '1', -8, 'c']
   const actual = part(someArray)(x => typeof x === 'number')
   const { pass, fail } = actual
@@ -14,5 +14,6 @@ test('The part function takes an array and returns a function that takes a filte
   t.equal(fail.includes('1'), true)
   t.equal(fail.includes('c'), true)
   t.equal(fail.length, 4)
+  t.equal(someArray.length, pass.length + fail.length)
   t.end()
 })
