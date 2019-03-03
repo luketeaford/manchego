6f035684208ba6efd2a9cc8522313cecbb801c84
Luke Teaford <luketeaford@gmail.com>
2019-03-02 19:05:53 -0500

    WIP Improve pair function

--- test/pair.test.js
+++ test/pair.test.js
@@ -20,46 +20,15 @@ test('The pair function returns an object with a single letter command set to tr
 })
 
 test('The pair function returns an object with commands declared with two dashes equal to true if no value is given.', t => {
-  const actual = pair(['--whatever', '--fine'])
+  const actual = pair(['--whatever', '--fine=ok'])
   t.equal(actual.whatever, true)
   t.end()
 })
 
-// ROUGH DRAFT
-// const pair = settings => {
-//   const obj = {}
-//   settings.forEach(cmd => {
-//     if (cmd.startsWith('--')) {
-//       const splitName = cmd.substring(2).split('=')
-//       obj[splitName[0]] = splitName[1] || true
-//     } else {
-//       if (cmd.startsWith('-')) {
-//         if (cmd.length > 1) {
-//           for (let i = 1; i < cmd.length; i += 1) {
-//             obj[cmd[i]] = true
-//           }
-//         }
-//         const nextValue = settings[settings.indexOf(cmd) + 1]
-//         obj[cmd.substring(1)] = nextValue.startsWith('-') ? true : nextValue
-//       }
-//     }
-//   })
-//   return obj
-// }
-
-// test('The pair function breaks command line arguments into key/value pairs.', t => {
-//   const actual = pair(['-c', 'cool', '--whatever=fine', '-x', '--sure'])
-//   t.equal(actual.c, 'cool')
-//   t.equal(actual.x, true)
-//   t.equal(actual.whatever, 'fine')
-//   t.equal(actual.sure, true)
-//   t.end()
-// })
-
-// test('The pair function allows short commands to be combined.', t => {
-//   const actual = pair(['-zRkbt', 'tomato'])
-//   console.log(actual)
-//   t.equal(actual.z, true)
-//   t.equal(actual.R, true)
-//   t.end()
-// })
+test('The pair function returns an object with commands declared with two dashes to the value passed on the right of the equals sign.', t => {
+  const actual = pair(['--bread=rye', '--foo', '--cheese=false'])
+  t.equal(actual.bread, 'rye')
+  t.equal(actual.foo, true)
+  t.equal(actual.cheese, false)
+  t.end()
+})
