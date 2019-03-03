28076f6743e4ad19115a3b8af4004f742dd52b36
Luke Teaford <luketeaford@gmail.com>
2019-03-03 08:20:41 -0500

    Use index as the main function

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-const pair = require('./lib/pair')
-const slice = require('./lib/slice')
+const part = require('./lib/part')
 
-const index = function (argv) {
-  return pair(slice(argv))
+const hasDashes = x => x.startsWith('-')
+const hasTwoDashes = x => x.startsWith('--')
+
+const index = argv => {
+  const arr = argv.slice(2)
+  const obj = {}
+  const objFn = n => Object.assign(obj, n)
+
+  const dashArrays = part(arr.filter(hasDashes))(hasTwoDashes)
+  const oneDashArr = dashArrays.fail
+  const twoDashArr = dashArrays.pass
+
+  const parseOneDash = x => {
+    const cmd = x.split('-')[1]
+    const nextValue = arr[arr.indexOf(x) + 1]
+    const isLong = cmd.length > 1
+
+    objFn({
+      [cmd[0]]: !nextValue || (nextValue && nextValue.startsWith('-'))
+        ? true
+        : oneDashArr.indexOf(x) === oneDashArr.length - 1
+          ? arr.slice(arr.indexOf(x) + 1).join(' ')
+          : nextValue
+    })
+
+    if (isLong) {
+      parseOneDash(`-${cmd.slice(1)}`)
+    }
+  }
+
+  oneDashArr.forEach(parseOneDash)
+
+  const parseTwoDashes = x => {
+    const cmd = x.split('--')[1]
+    const splitCmd = cmd.split('=')
+
+    objFn({
+      [splitCmd[0]]: splitCmd[1]
+        ? splitCmd[1] !== 'false' && splitCmd[1]
+        : true
+    })
+  }
+
+  twoDashArr.forEach(parseTwoDashes)
+
+  return obj
 }
 
 module.exports = index
