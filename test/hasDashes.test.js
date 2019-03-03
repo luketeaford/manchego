5e3a90f53193579a8c97b28f84021dd27abb70b4
Luke Teaford <luketeaford@gmail.com>
2019-03-03 07:54:01 -0500

    Write simple functions inline

--- test/hasDashes.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('tape')
-const hasDashes = require('../lib/hasDashes')
-
-test('The hasDashes function returns true if a string starts with "-".', t => {
-  t.equal(hasDashes('-whatever'), true)
-  t.equal(hasDashes('no'), false)
-  t.end()
-})
--- test/hasTwoDashes.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('tape')
-const hasTwoDashes = require('../lib/hasTwoDashes')
-
-test('The hasTwoDashes function returns true if a string starts with "--".', t => {
-  t.equal(hasTwoDashes('--ok'), true)
-  t.equal(hasTwoDashes('-still-no'), false)
-  t.end()
-})
